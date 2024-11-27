ace0f90ae9860c710c03e08a3a9a8667d2cb974d
Onixfff <60624718+Onixfff@users.noreply.github.com>
2024-11-27 15:50:41 +0200

    Добавил converter для настройки datagrid чтобы он занимал всё место

---
+++ TestFurstWPF/Converters/VisibilityToWidthConverter.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace TestFurstWPF.Converters
+{
+    
+    public class VisibilityToWidthConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Visibility visibility)
+            {
+                if (visibility == Visibility.Collapsed || visibility == Visibility.Hidden)
+                {
+                    return new GridLength(1, GridUnitType.Star);
+                }
+                else if(visibility == Visibility.Visible)
+                {
+                    return new GridLength(0, GridUnitType.Star);
+                }
+                else
+                {
+                    return new GridLength(2, GridUnitType.Star);
+                }
+            }
+            return new GridLength(0, GridUnitType.Star); // Значение по умолчанию
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
